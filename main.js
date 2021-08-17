012b7ea83adeafab335063062b8d539bc900aa90
Prince Bajagain <88591578+PrinceBajagain@users.noreply.github.com>
2021-08-17 22:50:19 +0545

    Javascript

---
+++ main.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>calculator</title>
+    <link rel="stylesheet" href="style.css">
+   </head>
+   <body>
+     <div id="all">                                               10     <div id="st"></div>
+      <div id="nd"></div>
+      <div id="rd"></div>
+      <div id="th"></div>                                        14     <div id="main"></div>
+     </div>                                                       16 </body>                                                        17 <script src="main.js"></script>
+</html>
